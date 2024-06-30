99a8b86a57cf105b6bf6f7ecfe8ba0a65d08949a
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-06-30 22:33:13 +0300

    HW 32 ConnectionPoll

--- src/telran/interviews/ConnectionPool.java
+++ src/telran/interviews/ConnectionPool.java
@@ -1,18 +1,33 @@
 package telran.interviews;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 //all methods should have O[1] complexity
 public class ConnectionPool {
-	//TODO Data Structure
+	LinkedHashMap<Long, Connection> map;
+	int size;
+	
+	@SuppressWarnings("serial")
 	public ConnectionPool(int size) {
-		//TODO
-	}
+		this.size = size;
+		this.map = new LinkedHashMap<Long, Connection>(10, 0.75f, true) {
+			@Override
+			protected boolean removeEldestEntry(Map.Entry<Long, Connection> entry) {
+				return size() > 3;
+				}
+			};
+			}
+	
 public Connection getConnection(Connection connection) {
-	//TODO return a connection from the pool if it exists
-	//otherwise creates new connection, adds in pool and returns new created connection
-	return null;
+	Connection res = map.get(connection.id());
+	if(res == null) {
+		res = connection;
+		map.put(connection.id(), connection);
+	}
+	return res;
 }
 public boolean isInPool(long id) {
-	//TODO returns true if  a given connection exists in the pool
-	//otherwise returns false
-	return false;
+	return map.containsKey(id);
 }
 }
