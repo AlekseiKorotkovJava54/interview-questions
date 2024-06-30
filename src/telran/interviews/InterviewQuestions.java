c32d62b1a9cbf628a141283ec311a620402887b4
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-07-01 00:45:05 +0300

    HW 32 getMaxWithNegativePresentation + isSum2 v2

--- src/telran/interviews/InterviewQuestions.java
+++ src/telran/interviews/InterviewQuestions.java
@@ -34,20 +34,20 @@ static public boolean isSum2(int [] array, int sum) {
 	boolean res = false;
 	HashSet<Integer> set = new HashSet<Integer>();
 	int index = 0;
-	int tmp;
-	int num;
 	while (!res && index<array.length) {
-		num = array[index++];
-		tmp = sum - num;
-		if(set.contains(tmp)) res = true;
-		else set.add(num);
+		if(set.contains(sum - array[index])) res = true;
+		else set.add(array[index]);
+		index++;
 	}
 	return res;
 }
 static public int getMaxWithNegativePresentation(int [] array) {
-	//TODO returns maximal positive value for which exists negative one with the same abs value
-	//if no pair of positive and negative values with the same abs value the method returns -1
-	//complexity O[N] only one pass over the elements
-	return -1;
+	int res = -1;
+	HashSet<Integer> set = new HashSet<Integer>();
+	for(int num: array) {
+		if(set.contains(0 - num)) res = Math.abs(num) > res ? Math.abs(num) : res;
+		set.add(num);
+	}
+	return res;
 }
 }
