fb19567e1e7bfc06a325016ad40e5bd462aba749
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-07-01 19:40:44 +0300

    HW 32 YG

--- src/telran/interviews/ConnectionPool.java
+++ src/telran/interviews/ConnectionPool.java
@@ -5,29 +5,25 @@ import java.util.Map;
 
 //all methods should have O[1] complexity
 public class ConnectionPool {
-	LinkedHashMap<Long, Connection> map;
-	int size;
-	
+	private LinkedHashMap<Long, Connection> pool;
 	@SuppressWarnings("serial")
 	public ConnectionPool(int size) {
-		this.size = size;
-		this.map = new LinkedHashMap<Long, Connection>(10, 0.75f, true) {
+		pool = new LinkedHashMap<>(16, 0.75f, true) {
 			@Override
 			protected boolean removeEldestEntry(Map.Entry<Long, Connection> entry) {
-				return size() > 3;
-				}
-			};
+				return size() > size;
 			}
-	
-public Connection getConnection(Connection connection) {
-	Connection res = map.get(connection.id());
-	if(res == null) {
-		res = connection;
-		map.put(connection.id(), connection);
+		};
 	}
-	return res;
+public Connection getConnection(Connection connection) {
+	//return a connection from the pool if it exists
+	//otherwise creates new connection, adds in pool and returns new created connection
+	long id = connection.id();
+	return pool.computeIfAbsent(id, k -> new Connection(id));
 }
 public boolean isInPool(long id) {
-	return map.containsKey(id);
+	// returns true if  a given connection exists in the pool
+	//otherwise returns false
+	return pool.containsKey(id);
 }
 }
--- src/telran/interviews/InterviewQuestions.java
+++ src/telran/interviews/InterviewQuestions.java
@@ -31,23 +31,29 @@ private static HashMap<String, Integer> getOccurrencesMap(String[] strings) {
 	return result;
 }
 static public boolean isSum2(int [] array, int sum) {
-	boolean res = false;
-	HashSet<Integer> set = new HashSet<Integer>();
+	//returns true if a given array contains two numbers, the summing of which
+	//equals a given 'sum' value
+	//complexity O[N] only one pass over the elements
+	HashSet<Integer> helper = new HashSet<>();
 	int index = 0;
-	while (!res && index<array.length) {
-		if(set.contains(sum - array[index])) res = true;
-		else set.add(array[index]);
-		index++;
+	while(index < array.length && !helper.contains(sum - array[index])) {
+		helper.add(array[index++]);
 	}
-	return res;
+	return index < array.length;
 }
 static public int getMaxWithNegativePresentation(int [] array) {
-	int res = -1;
-	HashSet<Integer> set = new HashSet<Integer>();
+	//returns maximal positive value for which exists negative one with the same abs value
+	//if no pair of positive and negative values with the same abs value the method returns -1
+	//complexity O[N] only one pass over the elements
+	int maxRes = -1;
+	HashSet<Integer> helper = new HashSet<>();
 	for(int num: array) {
-		if(set.contains(0 - num)) res = Math.abs(num) > res ? Math.abs(num) : res;
-		set.add(num);
+		if(helper.contains(-num)) {
+			maxRes = Math.max(maxRes, Math.abs(num));
+		} else {
+			helper.add(num);
+		}
 	}
-	return res;
+	return maxRes;
 }
 }
